057afde192bb00e520ad8f9b224bd001db028920
AkihiroYamashita <akatsuki.business1@gmail.com>
2022-06-03 12:12:45 +0900

    database.php追加

--- database.php
+++ database.php
@@ -2,6 +2,8 @@
 define('DB_HOST',   'localhost');
 define('DB_USER',   'root');
 define('DB_PASSWD',   'phpyama1213');
+define('DB_USER',   '');
+define('DB_PASSWD',   '');
 define('DB_NAME',   'diving_realtime_creature_map');
 
  ?>
